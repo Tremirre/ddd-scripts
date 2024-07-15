ee329627bfb3832ffef0047cc660b4d4b10aae6e
Bartosz Stachowiak <sbartekt@op.pl>
2024-07-16 00:41:33 +0200

    extend logging, skip processed files

--- parse-vidoes.py
+++ parse-vidoes.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import logging
 import pathlib
 import gdown
 import subprocess
@@ -7,65 +8,80 @@ import subprocess
 import patoolib
 
 
-tmp_dir = pathlib.Path("~/AppData/Local/Temp/ddd/out/").expanduser()
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+TMP_DIR = pathlib.Path("~/AppData/Local/Temp/ddd/out/").expanduser()
 URL_TEMPLATE = "https://drive.google.com/uc?id={}"
+OUT_FOLDER = pathlib.Path("videos")
 
 if __name__ == "__main__":
-    # tmp_dir.mkdir(parents=True, exist_ok=True)
-    # for file in tmp_dir.glob("*"):
-    #     file.unlink()
+    OUT_FOLDER.mkdir(parents=True, exist_ok=True)
+    TMP_DIR.mkdir(parents=True, exist_ok=True)
+
+    for file in TMP_DIR.glob("*"):
+        file.unlink()
+
     ids_path = sys.argv[1]
     with open(ids_path, "r") as f:
         ids = json.load(f)
-    print(f"Loaded {len(ids)} ids from {ids_path}")
+
+    logging.info(f"Loaded {len(ids)} ids from {ids_path}")
+
     for file_id in ids:
         url = URL_TEMPLATE.format(file_id)
-        output_file = tmp_dir / f"{file_id}.7z"
-        if not output_file.exists():
-            gdown.download(url, output=str(tmp_dir / f"{file_id}.7z"), quiet=False)
 
-        print(f"Extracting {file_id} to {tmp_dir}")
+        archive_file = TMP_DIR / f"{file_id}.7z"
+        exported_file = TMP_DIR / f"{file_id}.npz"
+        output_file = OUT_FOLDER / f"{file_id}.mp4"
+
+        if output_file.exists():
+            logging.info(f"Skipping {file_id} - already exists")
+            continue
+
+        if not archive_file.exists():
+            gdown.download(url, output=str(TMP_DIR / f"{file_id}.7z"), quiet=False)
+
+        logging.info(f"Extracting {file_id} to {TMP_DIR}")
         patoolib.extract_archive(
-            str(output_file), outdir=str(tmp_dir), verbosity=1, interactive=False
+            str(archive_file), outdir=str(TMP_DIR), verbosity=1, interactive=False
         )
-        output_file.unlink()
-        extracted_file = list(tmp_dir.glob("*.hdf5"))[0]
+        archive_file.unlink()
+        extracted_file = list(TMP_DIR.glob("*.hdf5"))[0]
 
-        res = subprocess.run(
+        logging.info("Starting export process".center(80, "="))
+        subprocess.run(
             [
                 "python",
                 "exporter.py",
                 "--input",
                 str(extracted_file),
                 "--output",
-                str(tmp_dir / f"{file_id}.npz"),
+                str(exported_file),
             ],
-            check=False,
-            capture_output=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=True,
         )
-        if res.returncode != 0:
-            print(f"Failed to export {file_id}: {res.stderr!r}")
-            exit(1)
 
-        print(f"Exported {file_id} to {tmp_dir / f'{file_id}.npz'}")
+        logging.info(f"Exported {file_id} to {exported_file}")
         extracted_file.unlink()
-
+        logging.info("Starting conversion process".center(80, "="))
         subprocess.run(
             [
                 "python",
                 "converter.py",
                 "--input",
-                str(tmp_dir / f"{file_id}.npz"),
+                str(exported_file),
                 "--output",
-                f"videos/{file_id}.mp4",
+                str(output_file),
             ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
             check=True,
-            capture_output=True,
         )
-        print(f"Converted {file_id} to videos/{file_id}.mp4")
-        (tmp_dir / f"{file_id}.npz").unlink()
+        logging.info(f"Converted {file_id} to videos/{file_id}.mp4")
+        exported_file.unlink()
 
-        exit(0)
-
-    print(tmp_dir.resolve())
-    print("Hello World")
+    logging.info("Done".center(80, "="))
